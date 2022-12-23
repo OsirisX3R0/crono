7c6410b118e8d28d582c4b51e354a21f95489933
Richard Harris <richardharris916@gmail.com>
2022-12-23 16:01:09 -0600

    Added modal for add form

--- src/components/jobs/Jobs.jsx
+++ src/components/jobs/Jobs.jsx
@@ -1,9 +1,10 @@
 import React, { useContext, useState } from "react";
+// import Modal from 'react-modal'
 import { GlobalContext } from "../../context/GlobalContext";
 import CronJob from "../../utils/CronJob";
-import Button from "../core/Button";
-
+import Modal from '../core/Modal'
 import Field from "../core/Field";
+import Button from "../core/Button";
 import Job from "./Job";
 
 const Jobs = () => {
@@ -25,21 +26,24 @@ const Jobs = () => {
 
   const jobList = jobs.map((job, i) => <Job key={i} job={job} />)
 
-  const addForm = adding ? (
-    <div>
+  const addForm = (
+    <Modal 
+      isOpen={adding}
+      onRequestClose={() => setAdding(false)}
+    >
       <Field label="Name" className="mb-2" value={name} onInput={(e) => setName(e.target.value)} />
       <Field label="Schedule" className="mb-2" value={schedule} onInput={(e) => setSchedule(e.target.value)} />
       <Button color="bg-orange-700" onClick={() => addJob()}>Add</Button>
-    </div>
-  ) : null
+    </Modal>
+  )
 
   return (
-    <div className="xs:h-64 sm:h-[89.5vh] overflow-y-scroll p-4">
+    <div className="xs:h-16 sm:h-[89.5vh] overflow-y-scroll p-4">
       <Button color="bg-emerald-700" onClick={() => setAdding(true)}>New Job</Button>
-      {addForm}      
       <ul className="xs:hidden xs:h-0 sm:h-auto sm:block">
         {jobList}
       </ul>
+      {addForm}
     </div>
   )
 }
