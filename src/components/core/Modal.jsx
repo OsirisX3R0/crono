1b30a56ec648ba1234796f8a932fed96cf1f2dc8
Richard Harris <richardharris916@gmail.com>
2022-12-24 04:17:55 -0600

    Fixed: schedule height

--- src/components/core/Modal.jsx
+++ src/components/core/Modal.jsx
@@ -2,15 +2,7 @@ import React from 'react'
 import ReactModal from 'react-modal'
 
 ReactModal.setAppElement(document.getElementById('root'));
-const modalStyles = {
-  content: {
-    maxWidth: '240px',
-    maxHeight: '240px',
-    backgroundColor: '#262626',
-    color: '#f8fafc',
-    margin: '75px auto 0'
-  }
-}
+
 const Modal = ({children, ...modalProps}) => {
   return (
     <ReactModal {...modalProps}>
